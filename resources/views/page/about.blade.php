4b4cc23b56052cb7b46c3bdfba055c694939046d
Tomasz Dul <tomaszjdul@gmail.com>
2018-02-07 22:51:15 +0100

    test 2

---
+++ resources/views/page/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts/app')
+
+@section('content')
+        <h1> ABOUT wtf</h1>
+@endsection
+  
---
+++ resources/views/page/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts/app')
+
+@section('content')
+        <h1> INDEX wtf</h1>
+@endsection
+  
